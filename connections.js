f12f8dcb9b1ce089170e659bd698faaf3f17f021
cbtharin <142699769+cbtharin@users.noreply.github.com>
2025-07-17 09:50:57 -0400

    Update connections.js

--- connections.js
+++ connections.js
@@ -1,1 +1,13 @@
+    function press(btn) {
+     const activeButtons = document.querySelectorAll(".button.active");
 
+    // If already active, allow toggle off
+    if (btn.classList.contains("active")) {
+      btn.classList.remove("active");
+    } 
+    // Otherwise, only activate if fewer than 4 are active
+    else if (activeButtons.length < 4) {
+      btn.classList.add("active");
+    } 
+    }
+ 
